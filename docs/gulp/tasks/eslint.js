efd2c8cf4a239c993c1095bf392ce2b92a74933c
Marty McGee <mcgee.marty@gmail.com>
2015-06-30 01:06:45 +0100

    remove /docs test app directory

--- docs/gulp/tasks/eslint.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var handleErrors = require('../util/handleErrors');
-
-gulp.task('eslint', shell.task([
-  '"../node_modules/.bin/eslint" ./material-ui/src'
-])).on('error', handleErrors);
